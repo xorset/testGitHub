62d5bb23a4c56c2c7a466b3b109280dd6ccd2acd
16004997 <16004997@PC-004571.uhi.ad.local>
2020-02-11 10:07:26 +0000

    Initial commit

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,13 @@
+package testgithub;
+
+
+public class TestGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World of java");
+    }
+    
+}
